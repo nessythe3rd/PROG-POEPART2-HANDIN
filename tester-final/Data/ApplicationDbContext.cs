522f847d09fd58473c9c9721d9fd82b941a24f84
Mogammad Salie <ST10214012@vcconnect.edu.za>
2024-10-18 22:19:49 +0200

    ij

--- tester-final/Data/ApplicationDbContext.cs
+++ tester-final/Data/ApplicationDbContext.cs
@@ -8,6 +8,6 @@ namespace tester_final.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 
         public DbSet<Student> Students { get; set; }
-        public DbSet<UploadDocument> UploadDocuments { get; set; } // Add this line
+        public DbSet<UploadDocument> UploadDocuments { get; set; }
     }
 }
